1e1bed6972aa550ac38735faa0672cd2806c763f
therealgherkhin <lscholz14@googlemail.com>
2022-08-26 12:35:51 +0200

    initial setup

---
+++ Dockerfile
@@ -0,0 +1,8 @@
+FROM python:3.8
+RUN mkdir /usr/src/app/
+COPY . /usr/src/app/
+WORKDIR /usr/src/app/
+EXPOSE 8080
+RUN pip install -r requirements.txt
+ENTRYPOINT ["streamlit", "run"]
+CMD ["Login.py"]
---
+++ Login.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+st.set_page_config(
+    page_title="Login",
+    page_icon="🔐",
+)
+
+st.markdown("<h1 style='text-align: center; color: white;'>Welcome to your Smart-Monitor</h1>", unsafe_allow_html=True)
+st.markdown("<h2 style='text-align: center; color: white;'>Here you can monitor ALL of your Smart-Home-Devices</h2>", unsafe_allow_html=True)
