8648737b17e8022f1b2c8b511dbd140de91a409d
therealgherkhin <lscholz14@googlemail.com>
2022-08-26 17:26:33 +0200

    session_states

--- pages/01_🏡 Home.py
+++ pages/01_🏡 Home.py
@@ -1,7 +1,12 @@
 import streamlit as st
 
+if 'user_authentication' not in st.session_state:
+    st.session_state['user_authentication'] = False
+
 st.set_page_config(
-    page_title="Home", 
+    page_title="Home-Dashboard", 
     page_icon="🏡",
     )
 
+if st.session_state['user_authentication'] == True:
+    st.write('Hello World!')
